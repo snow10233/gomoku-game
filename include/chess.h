4ed32bf6b84fa79fe5961caaafa429d5480b12ca
snow10233 <sung951023@gmail.com>
2026-03-25 22:54:26 +0800

    將position修改為chess 並新增判斷邊界函數

---
+++ include/chess.h
@@ -0,0 +1,24 @@
+#pragma once
+class Chess {
+private:
+  int x, xLeftLimit, xRightLimit;
+  int y, yLeftLimit, yRightLimit;
+
+public:
+  Chess(const int &xPosition = 0, const int &yPosition = 0)
+      : x(xPosition), y(yPosition) {}
+
+  int getX() const;
+
+  int getY() const;
+
+  bool isXVaild(const int &xPosition) const;
+
+  bool isYVaild(const int &yPosition) const;
+
+  bool setPosition(const int &xPosition, const int &yPosition);
+
+  void setXLimit(const int &left, const int &right);
+
+  void setYLimit(const int &left, const int &right);
+};
