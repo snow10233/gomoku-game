d016a884de89b5fa78bc8c3d9d5e7562cc5ba91c
snow10233 <sung951023@gmail.com>
2026-03-26 18:33:42 +0800

    完成console按下任意建清空畫面功能

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "board.h"
 #include <iostream>
+#include <limits> // 需要引入這個才能使用 numeric_limits
 using namespace std;
 
 const int boardSize = 15;
@@ -16,7 +17,7 @@ void everyRoundStartMessage(const Board &b) {
   cout << "請輸入位置 (行 列): ";
 }
 
-void clearScreen() {
+void clearConsole() {
 #ifdef _WIN32
   system("cls");
 #else
@@ -24,9 +25,23 @@ void clearScreen() {
 #endif
 }
 
+void pauseConsole() {
+    cout << "Press Enter key to continue...";
+    
+    // 1. 清空狀態 (預防前面的 cin 發生錯誤被鎖死)
+    cin.clear(); 
+    
+    // 2. 把緩衝區裡面的殘留物（包含 \n）全部清空
+    // 這行的意思是：忽略接下來的所有字元，直到遇到 \n 為止
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    // 3. 真正等待玩家按下 Enter
+    cin.get();
+}
+
 int main() {
   Board testB1{boardSize};
-  int x = 0, y = 0, c;
+  int x = 0, y = 0;
   while (everyRoundStartMessage(testB1), cin >> x >> y) {
     PutChessResult result = testB1.putChess(x, y);
     if (result == PutChessResult::ALL_RIGHT_ONE) {
@@ -34,12 +49,8 @@ int main() {
     } else if (result == PutChessResult::OVER_EDGE) {
       cout << "位置無效，請重新輸入！" << endl; // 超出15x15
     }
-
-    cout << "Press Enter key to continue..." << endl;
-    // cin.clear();
-    // c = getchar();
-    fgetc(stdin);
-    clearScreen();
+    pauseConsole();
+    clearConsole();
   }
   cout << testB1 << endl;
 }
