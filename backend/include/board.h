529625e3ca220eee4e02408230a6ba132b8b4f8b
snow10233 <sung951023@gmail.com>
2026-03-27 17:36:22 +0800

    將下棋狀態和勝負狀態做成函式

--- backend/include/board.h
+++ backend/include/board.h
@@ -12,6 +12,10 @@ enum class BattleResult { BLACK_WIN, WHITE_WIN, DRAW, CONTINUE };
 
 std::ostream &operator<<(std::ostream &os, const ChessPiece &c);
 
+std::ostream &operator<<(std::ostream &os, const PutChessResult &p);
+
+std::ostream &operator<<(std::ostream &os, const BattleResult &b);
+
 using dualChessPieceVector = std::vector<std::vector<ChessPiece>>;
 class Board {
   friend std::ostream &operator<<(std::ostream &os, const Board &board);
