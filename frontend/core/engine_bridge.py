069fe840a42c25a00fd328d9f678595b0b38b052
snow10233 <sung951023@gmail.com>
2026-03-29 18:14:59 +0800

    調整資料夾層級 並新增遊戲模式&行為輸入

--- frontend/core/engine_bridge.py
+++ frontend/core/engine_bridge.py
@@ -11,6 +11,28 @@ class GomokuEngine:
             bufsize=1  # 行緩衝模式 (非常重要，避免 C++ 的輸出卡在管線裡)
         )
 
+    def send_game_mode(self, game_mode):
+        """傳送遊戲模式給 C++ 引擎，並回傳結果"""
+        # 1. 把座標傳給 C++ (記得加換行符號)
+        self.process.stdin.write(f"{game_mode}\n")
+        self.process.stdin.flush()
+
+        # 2. 讀取 C++ 算完後吐出來的一行結果
+        output = self.process.stdout.readline().strip()
+        
+        return True if output == "SUCCESS" else False
+
+    def send_action(self, action):
+        """傳送行為給 C++ 引擎，並回傳結果"""
+        # 1. 把座標傳給 C++ (記得加換行符號)
+        self.process.stdin.write(f"{action}\n")
+        self.process.stdin.flush()
+
+        # 2. 讀取 C++ 算完後吐出來的一行結果
+        output = self.process.stdout.readline().strip()
+        
+        return True if output == "SUCCESS" else False
+
     def put_chess(self, col, row):
         """傳送座標給 C++ 引擎，並回傳結果"""
         # 1. 把座標傳給 C++ (記得加換行符號)
@@ -21,8 +43,8 @@ class GomokuEngine:
         output = self.process.stdout.readline().strip()
         
         # 3. 把字串切開，回傳給隊友的 GUI 使用
-        result, state = output.split(" ")
-        return result, state
+        result, state , ai_x, ai_y = output.split(" ")
+        return result, state, ai_x, ai_y
 
     def close(self):
         """關閉引擎"""
