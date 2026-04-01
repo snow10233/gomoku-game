391455cb47777b0b2b19403cf93ad2ae48c67855
snow10233 <sung951023@gmail.com>
2026-04-02 07:49:56 +0800

    將board過於不符的模塊移植到GameManager中

---
+++ backend/include/game_manager.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "ai_player.h"
+#include "board.h"
+#include "chess.h"
+#include "data_saver.h"
+
+class GameManager {
+  friend std::ostream &operator<<(std::ostream &os, const GameManager &game);
+private:
+  Board gameBoard;
+  DataSaver gameDatas;
+  AiPlayer aiPlayer;
+  ChessPiece currentPlayer;
+  BattleResult battleState;
+  Chess lastlyChess;
+
+  void refreshBattleState();
+
+  void changePlayer();
+
+public:
+  GameManager(int size = 15);
+
+  ChessPiece getCurrentPlayer() const;
+
+  BattleResult getBattleState() const;
+
+  std::pair<int, int> overTime();
+
+  std::pair<int, int> takeBackAMove();
+
+  std::string saveData();
+
+  void reset();
+
+  PutChessResult putChess(const int x, const int y);
+
+  std::pair<int, int> AiPutChess();
+
+  std::pair<int, int> takeBack();
+
+  void showBoard() const;
+};
